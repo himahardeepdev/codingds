5f8592c869467e8e7439e3851d30e5b5dbabd805
himahardeepdev <yournewemail@example.com>
2024-09-24 21:13:20 +0530

    detect the loop in list

---
+++ src/ds/linkedlists/singallinkedlist/Prog11.java
@@ -0,0 +1,62 @@
+package ds.linkedlists.singallinkedlist;
+
+
+public class Prog11 {
+    private NodeList head;
+    private static class NodeList {
+        private int data;
+        private NodeList next;
+
+        public NodeList(int data){
+            this.data = data;
+            this.next = null;
+        }
+        
+    }
+
+    public void printList(){
+        NodeList current = head;
+        while (current != null) {
+            System.out.print(current.data+"-->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public boolean detecLoopInSingalList(){
+        NodeList fastPrt = head;
+        NodeList slowPtr = head;
+        while (fastPrt != null && fastPrt.next != null) {
+            fastPrt = fastPrt.next.next;
+            slowPtr = slowPtr.next;
+            if(slowPtr == fastPrt){
+                return true;
+            }
+            
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Prog11 p11 = new Prog11();
+
+        p11.head = new NodeList(10);
+        NodeList second = new NodeList(20);
+        NodeList third = new NodeList(30);
+        NodeList fourth = new NodeList(40);
+        NodeList fifth = new NodeList(50);
+        NodeList sixth = new NodeList(60);
+
+        p11.head.next = second;
+        second.next = third;
+        third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        sixth.next = third;
+
+        // p11.printList();
+        boolean cehck = p11.detecLoopInSingalList();
+        System.out.println(cehck);
+
+    }
+}
