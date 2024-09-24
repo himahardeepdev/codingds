51d658e3733277c5af3611412807d7f74ab050cc
himahardeepdev <yournewemail@example.com>
2024-09-24 19:04:04 +0530

    remove dupilcate elemetns from sort list

--- src/ds/linkedlists/singallinkedlist/Prog8.java
+++ src/ds/linkedlists/singallinkedlist/Prog8.java
@@ -42,16 +42,35 @@ public class Prog8 {
         return mainRefPtr.data;
     }
 
+    public void removeDuplicateElements(){
+        NodeList currentNode = head;
+        while (currentNode != null && currentNode.next != null) {
+            if(currentNode.data == currentNode.next.data){
+                currentNode.next = currentNode.next.next;
+            }else{
+                currentNode = currentNode.next;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Prog8 p8 = new Prog8();
         p8.head = new NodeList(10);
-        NodeList second = new NodeList(20);
-        NodeList third = new NodeList(30);
-        NodeList fourth = new NodeList(40);
+        NodeList second = new NodeList(10);
+        NodeList third = new NodeList(20);
+        NodeList fourth = new NodeList(20);
+        NodeList fifth = new NodeList(30);
+        NodeList sixth = new NodeList(30);
         p8.head.next = second;
         second.next = third;
         third.next = fourth;
+        fourth.next = fifth;
+        fifth.next = sixth;
+        // p8.printList(p8.head);
+        // System.out.println(p8.findElementFromLast(p8.head, 2));
+        p8.removeDuplicateElements();
         p8.printList(p8.head);
-        System.out.println(p8.findElementFromLast(p8.head, 2));
+
+
     }
 }
