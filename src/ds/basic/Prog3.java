fa252cf7e2527cb875d5b4bf61f3539d3867ae82
himahardeepdev <yournewemail@example.com>
2024-12-14 21:54:13 +0530

    largest number of two number

---
+++ src/ds/basic/Prog3.java
@@ -0,0 +1,11 @@
+package ds.basic;
+
+public class Prog3 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a > b ? a : b;
+        System.out.println(c);
+
+    }
+}
