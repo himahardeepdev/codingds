c4918f7bad11e3e0ee3b1e6af23d20ef948f986c
himahardeepdev <yournewemail@example.com>
2024-12-14 21:51:25 +0530

    swap two numbers using without variable

---
+++ src/ds/basic/Prog2.java
@@ -0,0 +1,14 @@
+package ds.basic;
+
+public class Prog2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+"   "+b);
+        a = a+b;
+        b = a-b;
+        a = a-b;
+        System.out.println(a+"  "+b);
+    }
+    
+}
