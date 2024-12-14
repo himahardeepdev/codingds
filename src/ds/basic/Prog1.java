8e4c6f3ac22a2b8c05bcff36f7f7751b1e0db080
himahardeepdev <yournewemail@example.com>
2024-12-14 21:47:10 +0530

    swap two numbers

---
+++ src/ds/basic/Prog1.java
@@ -0,0 +1,13 @@
+package ds.basic;
+
+public class Prog1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a+"   "+b);
+        int c = a ;
+        a = b ;
+        b = c ;
+        System.out.println(a+"   "+b);
+    }
+}
