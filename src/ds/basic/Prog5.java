982fe9f460da0a397a8c06cf53b72719b5ef5672
himahardeepdev <yournewemail@example.com>
2024-12-14 22:00:44 +0530

    check wether it is Divisible by 7

---
+++ src/ds/basic/Prog5.java
@@ -0,0 +1,12 @@
+package ds.basic;
+
+public class Prog5 {
+
+    public static void main(String[] args) {
+        int a = 7 ;
+        String res = a%7 == 0 ? a+" is diviable ny 7 " : a+" is not divisable by 7 ";
+
+        System.out.println(res);
+        
+    }
+}
