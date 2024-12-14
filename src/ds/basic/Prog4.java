3d2098bef59652eb191c20ebc685947309f1dd6a
himahardeepdev <yournewemail@example.com>
2024-12-14 21:56:49 +0530

    odd and even number

---
+++ src/ds/basic/Prog4.java
@@ -0,0 +1,10 @@
+package ds.basic;
+
+public class Prog4 {
+    public static void main(String[] args) {
+        int a = 101;
+        String res = a% 2 == 0 ? "even" : "odd";
+        System.out.println(res);
+    }
+    
+}
